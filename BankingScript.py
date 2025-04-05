6b7cc36b680d994ccbdf708efb9d2d7d3912d377
Micro <Aadamxpmalik@gmail.com>
2025-04-05 14:03:32 +0100

    Added the route section

--- BankingScript.py
+++ BankingScript.py
@@ -5,4 +5,21 @@ from pywebio import start_server
 
 put_text("XP BANKING SYSTEM")
 
-# FOR WORK STREAM
+
+
+
+
+
+# This is the route section for the program
+if __name__ == '__main__':
+    routes = {
+        "login": user_login,
+        "signup": user_signup,
+        "logout": user_logout,
+        "home": home_page,
+        "profile": profile_page,
+        "product": product_page,
+        "payment_page": payment_page,
+        "password_reset": password_reset_page
+    }
+    start_server(routes, port=5000, debug=True)
