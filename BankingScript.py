645ab426bbac1423253eb1d0fc52704a0e84e29b
Micro <Aadamxpmalik@gmail.com>
2025-04-24 15:39:19 +0100

    Created the final page which allows you to transfer data such as money from account to payee.

--- BankingScript.py
+++ BankingScript.py
@@ -41,13 +41,19 @@ navbar = """<div class="fixed-bottom">
                     </li>
                     <li class="">
                         <a href="?app=product" class="nav-link">
+                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+</svg>
+                            Transfer</a>
+                    </li>
+                    <li class="">
+                        <a href="?app=product" class="nav-link">
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-boxes" viewBox="0 0 16 16">
   <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434zM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567zM7.5 9.933l-2.75 1.571v3.134l2.75-1.571zm1 3.134 2.75 1.571v-3.134L8.5 9.933zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567zm2.242-2.433V3.504L8.5 5.076V8.21zM7.5 8.21V5.076L4.75 3.504v3.134zM5.258 2.643 8 4.21l2.742-1.567L8 1.076zM15 9.933l-2.75 1.571v3.134L15 13.067zM3.75 14.638v-3.134L1 9.933v3.134z"/>
 </svg>
                             Product</a>
                     </li>
                 </ul>
-
         </div>
 
     </nav>
@@ -226,7 +232,7 @@ def user_signup():
 
     put_code(signup_data)
 
-    if " " in signup_data["FullName"]:  # this checks if there is an space in the Fullname
+    if " " in signup_data["FullName"]:  # this checks if there is a space in the Fullname
         toast("please dont put spaces in the full name")
         user_signup()
 
@@ -427,7 +433,7 @@ def profile_page():
 @config(theme="dark")
 def product_page():
     clear()
-    back_button("?app=payment_page")
+    back_button("?app=transfer_page")
     put_html(navbar)
     put_html("<h1>Product Page</h1>")
 
@@ -525,6 +531,11 @@ def password_reset_page():
         else:
             put_html("ERROR")
 
+@config(theme="dark")
+def transfer_page():
+    clear()
+    back_button("?app=payment_page")
+    put_html("<h1>Transfer Page Page</h1>")
 
 # This is the welcome hub where the user will start the journey of either signing in the account or logging in
 @config(theme="dark")
@@ -566,6 +577,7 @@ if __name__ == '__main__':
         "profile": profile_page,
         "product": product_page,
         "payment_page": payment_page,
-        "password_reset": password_reset_page
+        "password_reset": password_reset_page,
+        "transfer_page" : transfer_page
     }
     start_server(routes, port=5000, debug=True)
